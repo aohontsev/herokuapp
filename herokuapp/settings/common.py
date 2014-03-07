20e51174e6b3253ddb65347806bee9388fb43e26
aohontsev <anton.ohontsev@wikimart.ru>
2014-03-07 19:13:14 +0400

    Add bird, and setting staticfiles

--- herokuapp/settings/common.py
+++ herokuapp/settings/common.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -22,7 +21,7 @@ SECRET_KEY = 'a@j_m9$&@h7yx6osnzep5bx(%+1qrc%76bmaxyx&r%#m1_te9a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 
 # Application definition
@@ -77,14 +76,39 @@ USE_TZ = True
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 # Static asset configuration
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+SETTINGS_ROOT = os.path.dirname(__file__)
+
+STATIC_ROOT = os.path.join(SETTINGS_ROOT, 'static/')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+)
+
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    #'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'templates'),
 )
--- herokuapp/views.py
+++ herokuapp/views.py
@@ -2,7 +2,8 @@
 __author__ = 'aohontsev'
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Test-test-test")
+    return render(request, 'index.html')
